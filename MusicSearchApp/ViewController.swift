0234eaddb3928a83bf3e32f31ee7396ba376ef6c
Edgar <edgar.gonzalezpena@aviva.com>
2020-02-07 21:17:06 +0100

    Initial Commit

---
+++ MusicSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicSearchApp
+//
+//  Created by Edgar Gonzalez Pena on 07/02/2020.
+//  Copyright © 2020 Edgar Gonzalez Pena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
